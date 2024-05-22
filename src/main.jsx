68a0f389df3c2e8ef4459c2f762725f038c01243
chhavi6V <studychhavi03@gmail.com>
2024-05-22 08:19:23 +0530

    ga

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from "react-router-dom"
+import ReactGA from "react-ga4";
+
+useEffect(()=>{
+  ReactGA.initialize("G-KF9V9PEKQF");
+
+  ReactGA.preview(window.location.pathname + window.location.search)
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
