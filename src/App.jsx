9f8caa4cf826e1cc72eb5b2c0b3717c92f18d1a7
chhavi6V <studychhavi03@gmail.com>
2024-05-22 08:30:24 +0530

    ga2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Navigate, Route, Routes } from "react-router-dom"
 import Hello from './Hello'
 import Bye from './Bye'
 import Home from './Home'
 import ReactGA from "react-ga4";
 
-useEffect(()=>{
-  ReactGA.initialize("G-KF9V9PEKQF");
-
-  ReactGA.preview(window.location.pathname + window.location.search)
-})
-
 function App() {
-
+  useEffect(()=>{
+    ReactGA.initialize("G-KF9V9PEKQF");
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search, title: "Custom Title" });
+  })
   return (
     <Routes>
             <Route path="/" element={<Home/>}/>
